94375acb97a6c1d214c759cd4d6f9a5188208bd3
Cheetar <mateusz@sieniawski.net>
2020-10-30 13:14:33 +0100

    Describtion of translation algorithm

--- translate.py
+++ translate.py
@@ -41,8 +41,37 @@ def read_two_tape_transitions(path):
 
 
 def translate_transitions_to_one_tape(two_tape_transitions):
-    raise NotImplementedError
+    """Translates two tape Turing Machine to a single tape TM.
 
+    Both tapes will be stored on the same tape in format:
+    <first tape> # <second tape> #
+
+    General algorithm for transcribtion from two to single tape:
+    1. Underline the first letter of the input word. (underline = position
+       of the head)
+    2. Go to the end of the first tape and write #. Leave one underlined BLANK
+       representing the second tape and leave next #. Go back to the beginning
+       of the tape. Tape is now in format
+       <U_first_letter_of_input> <rest_of_input_word> # <U_BLANK> #.
+    3a. Make transition of the first head. Mark new head position.
+    3b. If the transition exceeds the first tape size, rewrite the second tape
+        one position to the left.
+    4. Go to the second head.
+    5. Make transition of the second head. If ends up on #, then rewrite it
+       one position to the left.
+    6. Go back to the position of the first head.
+    7. Go to the point 3a. of the algorithm.
+
+    Args:
+        two_tape_transitions ([two_tape_transition]): A two tape transition is
+            a tuple: <state> <let1> <let2> <target_state> <out_let1> <out_let2>
+            <dir1> <dir2>
+
+    Returns:
+        [transition]: List of single tape Turing Machine transitions.
+    """
+    # Step 1.
+    
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
