d59f7b45a9244c00ba05c328d24c71ec52e2ebd2
Cheetar <mateusz@sieniawski.net>
2020-10-31 21:01:24 +0100

    Commented translator.

--- translate.py
+++ translate.py
@@ -58,14 +58,19 @@ def translate_transitions_to_one_tape(TT_transitions):
        representing the second tape and leave next #. Go back to the beginning
        of the tape. Tape is now in format
        <U_first_letter_of_input> <rest_of_input_word> # <U_BLANK> #.
-    3a. Make transition of the first head. Mark new head position.
-    3b. If the transition exceeds the first tape size, rewrite the second tape
-        one position to the left.
-    4. Go to the second head.
-    5. Make transition of the second head. If ends up on #, then rewrite it
-       one position to the left.
-    6. Go back to the position of the first head.
-    7. Go to the point 3a. of the algorithm.
+    3. Read the letter under the first head.
+    4. Go to the second head and read the second letter.
+    5. Read the target state, letters and directions.
+    6. Execute the second head transition.
+    7. Go back to the first head.
+    8. Execute the first head transition.
+    9. If the new state is accept/reject, end the run.
+    10. Go to the point 3. of the algorithm.
+    
+    Position of the first head is underlined. Position of the second head is
+    double underlined. If the first head tries to go to the right, but there
+    is not enogh space, then the second tape is rewritten one position to the
+    right.
 
     Args:
         TT_transitions ([two_tape_transition]): A two tape transition is
@@ -81,9 +86,9 @@ def translate_transitions_to_one_tape(TT_transitions):
     underlined_alphabet = set(max_val + let for let in alphabet)
     double_underlined_alphabet = set(2*max_val + let for let in alphabet)
     SEPARATOR = 4 * max_val  # Separating the first and the second tape
-    OT_transitions = []
+    OT_transitions = []  # One Tape transitions
 
-    ################################################ 1.INITIALIZATION ################################################
+    # Initialization of the tape.
     OT_transitions += [(START_STATE, let, state("initializeFirstTape"), underline(let, max_val), R)
                         for let in alphabet]
     OT_transitions += [(state("initializeFirstTape"), let, state("initializeFirstTape"), let, R)
@@ -102,7 +107,8 @@ def translate_transitions_to_one_tape(TT_transitions):
     OT_transitions += [(state("goBackToFirstHeadOnFirstTape", org_state=org_state), u_let, state("ReadLet2", org_state=org_state, let1=un_underline(u_let, max_val)), u_let, R)
                           for u_let in underlined_alphabet
                           for org_state in states]
-    ################################################ EXECUTE FIRST HEAD ACTION ################################################
+
+    # Execute the second head transition.
     OT_transitions += [(state("ReadLet2", org_state=org_state, let1=let1), let, state("ReadLet2", org_state=org_state, let1=let1), let, R)
                           for let in alphabet | {SEPARATOR}
                           for let1 in alphabet
@@ -157,6 +163,8 @@ def translate_transitions_to_one_tape(TT_transitions):
                         for dir1 in DIRS
                         for let in alphabet
                         for org_state in states]
+    
+    # Go back to the first head.
     OT_transitions += [(state("goToFirstHead", org_state=org_state, tlet1=tlet1, dir1=dir1), let, state("goToFirstHead", org_state=org_state, tlet1=tlet1, dir1=dir1), let, L)
                     for tlet1 in alphabet
                     for dir1 in DIRS
@@ -167,6 +175,8 @@ def translate_transitions_to_one_tape(TT_transitions):
                     for dir1 in DIRS
                     for let1 in underlined_alphabet
                     for org_state in states]
+    
+    # Execute the first head transition.
     OT_transitions += [(state("executeFirstHeadAction", org_state=org_state, tlet1=tlet1, dir1=S), let1, state("checkIfTerminalState", org_state=org_state), underline(tlet1, max_val), S)
                     for tlet1 in alphabet
                     for let1 in underlined_alphabet
@@ -212,7 +222,8 @@ def translate_transitions_to_one_tape(TT_transitions):
     OT_transitions += [(state("goToFirstHeadCheckTerminal", org_state=org_state), let, state("checkIfTerminalState", org_state=org_state), let, S)
                        for let in underlined_alphabet
                        for org_state in states]
-    # Check if terminal state
+
+    # Check if terminal state.
     OT_transitions += [(state("checkIfTerminalState", org_state=org_state), let1, org_state, let1, S)
                     for let1 in underlined_alphabet
                     for org_state in (ACCEPT_STATE, REJECT_STATE)]
@@ -223,23 +234,68 @@ def translate_transitions_to_one_tape(TT_transitions):
 
 
 def underline(let, max_val):
+    """Returns the underlined value of a letter.
+
+    Args:
+        let (int): Tape letter.
+        max_val (int): Maximum value used by the TM +1
+
+    Returns:
+        int: Encoded underlined letter.
+    """
     return max_val + let
 
 
 def double_underline(let, max_val):
+    """Returns the double underlined value of a letter.
+
+    Args:
+        let (int): Tape letter.
+        max_val (int): Maximum value used by the TM +1
+
+    Returns:
+        int: Encoded double underlined letter.
+    """
     return 2 * max_val + let
 
 
 def un_underline(let, max_val):
+    """Decodes value of the original letter from its underlined value.
+
+    Args:
+        let (int): Underlined tape letter.
+        max_val (int): Maximum value used by the TM +1
+
+    Returns:
+        int: Decoded value of the original letter.
+    """
     return let - max_val
 
 
 def un_double_underline(let, max_val):
+    """Decodes value of the original letter from its double underlined value.
+
+    Args:
+        let (int): Double underlined tape letter.
+        max_val (int): Maximum value used by the TM +1
+
+    Returns:
+        int: Decoded value of the original letter.
+    """
     return let - 2 * max_val
 
 
 def get_alphabet(TT_transitions):
-    alphabet = {*range(3)}
+    """Obtain set of tape letters used by a Turing Machine.
+
+    Args:
+        TT_transitions (two_tape_transisions): Dictionary
+            (state, let1, let2) => (t_state, t_let1, t_let2, dir1, dir2)
+
+    Returns:
+        set(int): Set of all letters used by the TM.
+    """
+    alphabet = {BLANK}
     for ((_, cur_let1, cur_let2), TT_transition_results) in TT_transitions.items():
         alphabet |= {cur_let1, cur_let2}
         for (_, out_let1, out_let2, _, _) in TT_transition_results:
@@ -248,6 +304,15 @@ def get_alphabet(TT_transitions):
 
 
 def get_states(TT_transitions):
+    """Get all states used by the TM.
+
+    Args:
+        TT_transitions (two_tape_transisions): Dictionary
+            (state, let1, let2) => (t_state, t_let1, t_let2, dir1, dir2)
+
+    Returns:
+        set(str): Set of all states used by the TM.
+    """
     states = {START_STATE, ACCEPT_STATE, REJECT_STATE}
     for ((org_state, _, _), TT_transition_results) in TT_transitions.items():
         states |= {org_state}
@@ -257,6 +322,14 @@ def get_states(TT_transitions):
 
 
 def state(name, **kwards):
+    """Encodes the TM state.
+
+    Args:
+        name (str): State name
+
+    Returns:
+        str: Encoded state.
+    """
     out = f"{name}"
     for key, val in kwards.items():
         out += f"|{key}:{val}"
